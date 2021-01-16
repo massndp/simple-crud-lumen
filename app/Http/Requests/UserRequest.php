a619e26f0f0d68ab3809283903e2d6b6f93ec979
massndp <bayu8989.88@gmail.com>
2021-01-16 14:17:24 +0700

    Learn Lumen API

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Anik\Form\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    protected function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    protected function rules()
+    {
+        return [
+            'name'=>'required|max:255',
+            'email'=>'required|max:255',
+            'password'=>'required|max:255'
+        ];
+    }
+}
